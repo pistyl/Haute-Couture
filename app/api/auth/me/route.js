e2edb7999dcfa5f0e4c177c08652d005eaf1e792
Ibrahima Samb <i.s.papy97@gmail.com>
2026-07-17 22:29:43 +0000

    fix: force dynamic rendering on all GET endpoints using getUserSession to prevent DYNAMIC_SERVER_USAGE build error

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -1,6 +1,8 @@
 import { query } from '../../../../lib/db';
 import { getUserSession } from '../../../../lib/auth';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request) {
   try {
     const userId = getUserSession(request);
